e223253bb8cb9e037e58d615284e0f73ffb05bd6
den4j <denis.kislukhin@gmail.com>
2012-11-09 09:32:45 +0400

    Routes for robokassa billing integration corrected

--- spree_robokassa/config/routes.rb
+++ spree_robokassa/config/routes.rb
@@ -1,7 +1,7 @@
 Spree::Core::Engine.routes.draw do
 	scope 'robokassa' do
-		post 'paid'    => 'robokassa#paid',    :as => :robokassa_paid # to handle Robokassa push request
-		post 'success' => 'robokassa#success', :as => :robokassa_success # to handle Robokassa success redirect
-		post 'fail'    => 'robokassa#fail',    :as => :robokassa_fail # to handle Robokassa fail redirect
+		match 'paid'    => 'robokassa#paid',    :as => :robokassa_paid, :via => [:get, :post]
+		match 'success' => 'robokassa#success', :as => :robokassa_success, :via => [:get, :post]
+		match 'fail'    => 'robokassa#fail',    :as => :robokassa_fail, :via => [:get, :post]
 	end
 end
