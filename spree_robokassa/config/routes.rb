2649e47b69381b06be04eb3e407db8b5309c497f
den4j <denis.kislukhin@gmail.com>
2012-11-09 09:20:10 +0400

    minor fixes

--- spree_robokassa/config/routes.rb
+++ spree_robokassa/config/routes.rb
@@ -1,7 +1,7 @@
 Spree::Core::Engine.routes.draw do
 	scope 'robokassa' do
-		match 'paid'    => 'robokassa#paid',    :as => :robokassa_paid # to handle Robokassa push request
-		match 'success' => 'robokassa#success', :as => :robokassa_success # to handle Robokassa success redirect
-		match 'fail'    => 'robokassa#fail',    :as => :robokassa_fail # to handle Robokassa fail redirect
+		post 'paid'    => 'robokassa#paid',    :as => :robokassa_paid # to handle Robokassa push request
+		post 'success' => 'robokassa#success', :as => :robokassa_success # to handle Robokassa success redirect
+		post 'fail'    => 'robokassa#fail',    :as => :robokassa_fail # to handle Robokassa fail redirect
 	end
 end
